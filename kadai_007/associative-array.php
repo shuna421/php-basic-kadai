42ae5db490ab6af2fe16e4fe93abcedfed410c3a
shuna421 <shuna2001@icloud.com>
2025-06-10 20:04:55 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>課題</title>
+</head>
+<body>
+  <p>
+    <?php
+    $data=['name'=>'onion','price'=>'200','weight'=>'160'];
+
+    print_r($data);
+
+    ?>
+    </p>
+</body>
+</html>
